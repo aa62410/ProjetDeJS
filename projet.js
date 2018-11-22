27794c7604745bd74a54697a2f8b9c521fb902ad
aa62410 <aa.bourdon@laposte.net>
2018-11-22 22:42:57 +0100

    PDF : Création du Modèle. Par contre,  object MODEL pas sur que c'est comme ça

--- projet.js
+++ projet.js
@@ -1,26 +1,76 @@
-function Point(abs = 0,ord = 0){
-    Object.Properties(this,{
-        x : {value : abs, writable : false},
-        y : {value : ord, writable : false}
+function Point(abs = 0, ord = 0) {
+    Object.defineProperty(this, "x", {
+        enumerable: false,
+        writable: false,
+        configurable: false,
+        value: abs
+    });
+    Object.defineProperty(this, "y", {
+        enumerable: false,
+        writable: false,
+        configurable: false,
+        value: ord
+    });
+};
+
+Point.prototype.horizontalSymmetry = function (n) {
+    'use strict';
+    return new Point(this.x, (this.y - (2 * (this.y - n))));
+};
+
+Point.prototype.verticalSymmetry = function (n) {
+    'use strict';
+    return new Point(this.x - (2 * (this.x - n)), this.y);
+};
+
+Point.prototype.average= function (p2,alpha){
+    'use strict';
+    return new Point(this.x*(1-alpha)+p2.x+alpha,this.y*(1-alpha)+p2.y+alpha);
+};
+
+Point.prototype.clone = function () {
+    'use strict';
+    return new Point(this.x, this.y);
+};
+
+function Segment(p1, p2, couleur) {
+    'use strict';
+    Object.defineProperty(this, "p1", {
+        enumerable: false,
+        writable: false,
+        configurable: false,
+        value: p1
+    });
+    Object.defineProperty(this, "p2", {
+        enumerable: false,
+        writable: false,
+        configurable: false,
+        value: p2
+    });
+    Object.defineProperty(this, "couleur", {
+        enumerable: false,
+        writable: false,
+        configurable: false,
+        value: tab
     });
 }
 
-Point.prototype.horizontalSymetry = function(n){
-    return new Point(this.x,((n.y - this.y)+n.y));
-}
+Segment.prototype.horizontalSymmetry = function (n) {
+    'use strict';
+    return new Segment(this.p1.horizontalSymmetry(n), this.p2.horizontalSymmetry(n));
+};
 
-Point.prototype.verticalSymetry = function(n){
-    return new Point(((n.x - this.x)+n.x),this.y);
-}
+Segment.prototype.verticalSymmetry = function (n) {
+    'use strict';
+    return new Segment(this.p1.verticalSymmetry(n), this.p2.verticalSymmetry(n));
+};
 
-Point.prototype.average= function (p2,alpha){
-    return new Point(this.x*(1-alpha)+p2.x+alpha,this.y*(1-alpha)+p2.y+alpha);
-}
+Segment.prototype.average = function (p, alpha) {
+    'use strict';
+    return new Segment(this.p1.average(p, alpha), this.p2.average(p, alpha));
+};
 
-Point.prototype.clone = function(){
-    return new Point(this.x,this.y);
-}
-
-function Segment(p1,p2,couleur = [0,0,0]){
-
+function Model (fen1 = [], fen2 []) {
+    this.fen1 = fen1;
+    this.fen2 = fen2;
 }
